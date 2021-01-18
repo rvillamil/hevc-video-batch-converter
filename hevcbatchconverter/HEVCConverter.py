24064b3be5490d594115c0fcabb61c9f487c02b8
Rodrigo <rodrigo.villamil@gmail.com>
2021-01-18 08:50:50 +0100

    Text output enhacenment

--- hevcbatchconverter/HEVCConverter.py
+++ hevcbatchconverter/HEVCConverter.py
@@ -25,9 +25,10 @@ def pretty_print(text):
 class HEVCConverter:
     """ API for HEVCConverter
     """
-    XMP_EXTENSION   = "xmp"
-    VIDEO_EXTENSION = "avi"
-    XML_NODE_REGEX  = r"photoshop:DateCreated>(.*)</photoshop:DateCreated"
+    XMP_EXTENSION           = "xmp"
+    OUTPUTFILE_EXTENSION    = "mp4"
+    OUTPUT_DIR              = "output"
+    XML_NODE_REGEX          = r"photoshop:DateCreated>(.*)</photoshop:DateCreated"    
 
     def print_all_convertible_files_in_dir(self, dir):
         os.chdir(dir)
@@ -50,48 +51,55 @@ class HEVCConverter:
                 creation_date = match.group(group_num)
         return creation_date
 
+    def detect_input_file_extension (self,filename_without_extension):
+        files_list=glob.glob( ("%s.*" % filename_without_extension)  )
+        files_list.remove (filename_without_extension + '.' + self.XMP_EXTENSION)
+        return (files_list[0].split('.')[1])
+
     def datetime_str_to_datetime(self, my_date_time_str):
         my_datetime_object = ciso8601.parse_datetime(my_date_time_str)
         return my_datetime_object.strftime("%m/%d/%Y %H:%M:%S")
 
-    def change_creation_date_on_macos(self, dirname, filename, new_creation_datetime):
+    def change_creation_date_on_macos(self, full_path_outputfile, new_creation_datetime):
         from subprocess import call
-        full_path_file = dirname + os.sep + filename
-        pretty_print("Setting creation date on file '%s'" % full_path_file)
-        command = 'SetFile -d "' + new_creation_datetime + '" "' + full_path_file + '"'
+        pretty_print("Setting creation date on file '%s'" % full_path_outputfile)
+        command = 'SetFile -d "' + new_creation_datetime + '" "' + full_path_outputfile + '"'
         _logger.debug("Running command '%s'" % command)
         call(command, shell=True)
 
-    def ffmep_convert_file(self, dirname, input_filename):
+    def ffmepg_convert_file(self, dirname, filename_without_extension, input_file_extension):
         # See: https://aaron.cc/ffmpeg-hevc-apple-devices/
         #   ffmpeg -i input.avi -c:v libx265 -crf 28 -c:a aac -b:a 128k -tag:v hvc1 output.mp4
         from subprocess import call
-        output_filename = input_filename.replace('.avi', '.mp4')
+        input_filename = filename_without_extension + '.' + input_file_extension
+        output_filename = filename_without_extension + '.' + self.OUTPUTFILE_EXTENSION
         full_path_imputfile = dirname + os.sep + input_filename
-        full_path_outputfile = dirname + os.sep + output_filename
+        full_path_outputfile = dirname + os.sep + self.OUTPUT_DIR + os.sep + output_filename
         pretty_print("Converting file '%s' to file '%s'" % (input_filename,output_filename))        
         command = "ffmpeg -i '{full_path_imputfile}' -c:v libx265 -crf 0 -c:a aac -b:a 128k -tag:v hvc1 '{full_path_outputfile}'".format(
             full_path_imputfile=full_path_imputfile, full_path_outputfile=full_path_outputfile)
         _logger.debug("Running comamnd '%s'" % command)
         call(command, shell=True)
-        return output_filename
+        return full_path_outputfile
 
-    def process_file(self, dir, file, creation_date_str):
-        _logger.debug("Converting file '{file}' and setting '{creation_date_str}' as creation date".format(
-            file=file, creation_date_str=creation_date_str))
+    def convert_file_from_dir(self, dir, input_file_extension, filename_without_extension, creation_date_str):
+        input_filename = filename_without_extension + "." + input_file_extension, 
+        _logger.debug("Converting file '{input_file}' and setting '{creation_date_str}' as creation date".format(
+            input_file=input_filename, creation_date_str=creation_date_str))
         new_creation_datetime = self.datetime_str_to_datetime(
             creation_date_str)
-        _logger.debug("New Date time %s" % new_creation_datetime)
-        # Set fecha de creacion
-        output_filename = self.ffmep_convert_file(dir, file)
-        self.change_creation_date_on_macos(
-            dir, output_filename, new_creation_datetime)
+        _logger.debug("New Date time %s" % new_creation_datetime)        
+        full_path_outputfile = self.ffmepg_convert_file(dir, filename_without_extension, input_file_extension)
+        self.change_creation_date_on_macos( full_path_outputfile, new_creation_datetime)
 
     def convert_dir(self, dir):
         os.chdir(dir)
-        for file in glob.glob("*." + self.XMP_EXTENSION):
+        for xmp_file in glob.glob("*." + self.XMP_EXTENSION):
             print("------------------------------------")
-            creation_date_str = self.extract_creation_date_from_xmp_file(file)
-            filename, extension = os.path.splitext(file)
-            self.process_file(dir, filename + "." +
-                              self.VIDEO_EXTENSION, creation_date_str)
+            filename_without_extension, extension = os.path.splitext(xmp_file)
+            creation_date_str       = self.extract_creation_date_from_xmp_file(xmp_file)
+            input_file_extension    = self.detect_input_file_extension(filename_without_extension)            
+            self.convert_file_from_dir(dir, 
+                                       input_file_extension,
+                                       filename_without_extension, 
+                                       creation_date_str)
